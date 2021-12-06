2099453f4818de75eff583bbd18a0a70bb99681a
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2021-12-06 09:16:38 +0700

    Fixed hooks

--- frontend/src/hooks.js
+++ frontend/src/hooks.js
@@ -9,7 +9,6 @@ export const handle = async ({ request, resolve }) => {
 	if (!cookies.session_id) {
 		request.locals.user.authenticated = false;
 	} else {
-<<<<<<< HEAD
 		request.locals.user.authenticated = true
 		let rows = await db.execute("SELECT * FROM session WHERE session = ?", [cookies.session_id]);
 		if(!rows[0][0]) {
@@ -21,15 +20,6 @@ export const handle = async ({ request, resolve }) => {
 			request.locals.user.info = JSON.parse(rows[0][0].info);
 			request.locals.user.about = rows[0][0].about;
 		}
-=======
-		request.locals.user.authenticated = true;
-		let rows = await db.execute('SELECT * FROM session WHERE session = ?', [cookies.session_id]);
-		rows = await db.execute('SELECT * FROM user WHERE id = ?', [rows[0][0].id]);
-		request.locals.user.id = rows[0][0].id;
-		request.locals.user.username = rows[0][0].username;
-		request.locals.user.info = JSON.parse(rows[0][0].info);
-		request.locals.user.about = rows[0][0].about;
->>>>>>> 66ae920ceffbf5933a89c01f34a1558240fb0d7b
 	}
 
 	const res = await resolve(request);
