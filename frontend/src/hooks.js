3951f8eebf82af83b32b2b70a22875326bdc6d23
kidjanate <kidjanate@gmail.com>
2021-12-05 18:17:18 -0800

    remove server.js

--- frontend/src/hooks.js
+++ frontend/src/hooks.js
@@ -10,8 +10,8 @@ export const handle = async ({ request, resolve }) => {
 		request.locals.user.authenticated = false;
 	} else {
 		request.locals.user.authenticated = true;
-		let rows = await db.execute('SELECT * FROM session WHERE session = ?', [cookies.session_id]);
-		rows = await db.execute('SELECT * FROM user WHERE id = ?', [rows[0][0].id]);
+		let selectId = await db.execute('SELECT * FROM session WHERE session = ?', [cookies.session_id]);
+		let rows = await db.execute('SELECT * FROM user WHERE id = ?', [selectId[0][0].id]);
 		request.locals.user.id = rows[0][0].id;
 		request.locals.user.username = rows[0][0].username;
 		request.locals.user.info = JSON.parse(rows[0][0].info);
@@ -25,17 +25,19 @@ export const handle = async ({ request, resolve }) => {
 };
 
 export const getSession = async (request) => {
-	if (!request.locals.user.authenticated) {
-		return {
-			authenticated: false
-		};
-	}
-
-	return {
-		authenticated: true,
-		id: request.locals.user.id,
-		username: request.locals.user.username,
-		info: request.locals.user.info,
-		about: request.locals.user.about
-	};
+    if(request.locals.user) {
+        if (!request.locals.user.authenticated) {
+            return {
+                authenticated: false
+            };
+        }
+    
+        return {
+            authenticated: true,
+            id: request.locals.user.id,
+            username: request.locals.user.username,
+            info: request.locals.user.info,
+            about: request.locals.user.about
+        };
+    }
 };
--- server.js
+++
@@ -1,7 +0,0 @@
-// Connect Express with Svelte
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-const path = require("path");
-const svelte = require("svelte/compiler");
